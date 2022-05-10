754d8b003c48624d944ac2cd0df3d347f17c7d39
Chris Heppell <chris.john.heppell@gmail.com>
2022-05-10 08:47:32 +0100

    add in-memory user repository

---
+++ http-server/user/repository.go
@@ -0,0 +1,92 @@
+package user
+
+import (
+	"fmt"
+	"sync"
+)
+
+type UserId string
+
+var (
+	UserNotFound      = fmt.Errorf("user could not be found")
+	UserAlreadyExists = fmt.Errorf("user alread exists")
+)
+
+type User struct {
+	Id               UserId
+	IdentityVerified bool
+	passbaseKey      *string
+}
+
+type userRepository struct {
+	mutex sync.RWMutex
+	users map[UserId]User
+}
+
+func NewUserRepository() userRepository {
+	return userRepository{
+		mutex: sync.RWMutex{},
+		users: make(map[UserId]User),
+	}
+}
+
+func (u *userRepository) CreateIfNotExist(userId UserId) (User, error) {
+	newUser := User{
+		Id:               userId,
+		IdentityVerified: false,
+		passbaseKey:      nil,
+	}
+	u.mutex.Lock()
+	defer u.mutex.Unlock()
+
+	if user, exists := u.users[userId]; exists {
+		return user, nil
+	}
+
+	u.users[userId] = newUser
+
+	return newUser, nil
+}
+
+func (u *userRepository) GetUserById(userId UserId) (*User, error) {
+	u.mutex.RLock()
+	defer u.mutex.RUnlock()
+	if user, exists := u.users[userId]; exists {
+		return &user, nil
+	}
+	return nil, UserNotFound
+}
+
+func (u *userRepository) GetUserFromPassbaseKey(passbaseKey string) (*User, error) {
+	u.mutex.RLock()
+	defer u.mutex.RUnlock()
+	for _, user := range u.users {
+		if user.passbaseKey == &passbaseKey {
+			return &user, nil
+		}
+	}
+	return nil, UserNotFound
+}
+
+func (u *userRepository) RegisterUserVerified(userId UserId) error {
+	u.mutex.RLock()
+	defer u.mutex.RUnlock()
+	if user, exists := u.users[userId]; exists {
+		user.IdentityVerified = true
+		u.users[userId] = user
+	}
+	return UserNotFound
+}
+
+func (u *userRepository) AssociatePassbaseKey(userId UserId, passbaseKey string) error {
+	u.mutex.Lock()
+	defer u.mutex.Unlock()
+	if user, exists := u.users[userId]; exists {
+		user.passbaseKey = &passbaseKey
+		u.users[userId] = user
+	} else {
+		return UserNotFound
+	}
+
+	return nil
+}
